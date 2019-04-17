a5e1b018ff7856678765f5419cda33f22f5e7292
Cédric Belin <cedric@belin.io>
2019-04-18 00:20:31 +0200

    Moved the documentation settings

--- RoboFile.php
+++ RoboFile.php
@@ -43,9 +43,9 @@ class RoboFile extends Tasks {
       ->addTask($this->taskFilesystemStack()
         ->copy('CHANGELOG.md', 'doc/about/changelog.md')
         ->copy('LICENSE.md', 'doc/about/license.md'))
-      ->addTask($this->taskExec('mkdocs build --config-file=doc/mkdocs.yml'))
+      ->addTask($this->taskExec('mkdocs build --config-file=etc/mkdocs.yml'))
       ->addTask($this->taskFilesystemStack()
-        ->remove(['doc/about/changelog.md', 'doc/about/license.md', 'web/mkdocs.yml', 'web/phpdoc.xml']))
+        ->remove(['doc/about/changelog.md', 'doc/about/license.md']))
       ->run();
   }
 
